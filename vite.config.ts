eebe6e416a1ad970304e1b4677ea77b70f52ef39
lacerezaibague-hash <lacerezaibague@gmail.com>
2025-11-19 02:59:24 -0500

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
   define: {
     // Use logical OR to ensure we never pass 'undefined' to JSON.stringify
     // which would result in the variable being undefined in the client code.
-    'process.env.API_KEY': JSON.stringify(process.env.API_KEY || ""),
     'process.env.SUPABASE_URL': JSON.stringify(process.env.SUPABASE_URL || ""),
     'process.env.SUPABASE_ANON_KEY': JSON.stringify(process.env.SUPABASE_ANON_KEY || "")
   }
