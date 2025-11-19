cfca6ee8f32f3bcfffef78d681f658f70303799f
lacerezaibague-hash <lacerezaibague@gmail.com>
2025-11-19 06:00:09 -0500

    vite-env.d.ts

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,9 +1,10 @@
 /// <reference types="vite/client" />
 
-declare namespace NodeJS {
-  interface ProcessEnv {
-    SUPABASE_URL: string;
-    SUPABASE_ANON_KEY: string;
-    [key: string]: string | undefined;
-  }
+interface ImportMetaEnv {
+  readonly VITE_SUPABASE_URL: string
+  readonly VITE_SUPABASE_ANON_KEY: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
 }
