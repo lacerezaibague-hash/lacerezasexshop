757a2bd621c1478ff6e933c33ba8ce2be6db5ba8
lacerezaibague-hash <lacerezaibague@gmail.com>
2025-11-19 05:59:43 -0500

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    // Use logical OR to ensure we never pass 'undefined' to JSON.stringify
-    // which would result in the variable being undefined in the client code.
-    'process.env.SUPABASE_URL': JSON.stringify(process.env.SUPABASE_URL || ""),
-    'process.env.SUPABASE_ANON_KEY': JSON.stringify(process.env.SUPABASE_ANON_KEY || "")
-  }
 })
